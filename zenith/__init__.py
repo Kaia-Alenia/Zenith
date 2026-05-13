772eb8d4f30494fcc8123fb6fca1a1d21ead31f2
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-05-12 20:24:38 -0600

    feat: add Alenia Studios ASCII banner and version bump to v0.1.2

--- zenith/__init__.py
+++ zenith/__init__.py
@@ -3,11 +3,25 @@ from .hooks.loader import install_hook
 from .speculation.predictor import predictor
 import atexit
 
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 _engine = SpeculationEngine()
+_ignited = False
 
 def ignite():
+    global _ignited
+    if not _ignited:
+        banner = """\033[95m
+      _   _ _____ _   _ ___ _____ _   _ 
+     / \\ | | ____| \\ | |_ _|_   _| | | |
+    / _ \\| |  _| |  \\| || |  | | | |_| |
+   / ___ \\ | |___| |\\  || |  | | |  _  |
+  /_/   \\_\\_____|_| \\_|___|  |_| |_| |_|
+         S T U D I O S   X   Z E N I T H
+\033[0m"""
+        print(banner)
+        _ignited = True
+
     modulos_precarga = predictor.load_predictions()
     for mod in modulos_precarga:
         _engine.register_module(mod)
